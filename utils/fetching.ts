c49b3693fa62b68e56acab126131959318a20d36
Quang Hung Pham <hungpham307@gmail.com>
2022-10-14 11:40:40 +0700

    chore: try external api

--- utils/fetching.ts
+++ utils/fetching.ts
@@ -1,5 +1,7 @@
 export async function fetchCorrespondingData<Type>(property: string) {
-  const res = await fetch("http://localhost:3000/data.json");
+  const res = await fetch(
+    "https://raw.githubusercontent.com/pqhung3007/fem-space-tourism/main/public/data.json"
+  );
   const data = await res.json();
   // get keyof (typeof data: object) : destinations/crew/technology
   const correspondingData: Type[] = data[property as keyof typeof data];
